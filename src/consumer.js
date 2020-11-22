2d024aa37251799e2932b6caa3d2a2df1345891f
mnmallea <mnmallea@gmail.com>
2020-11-22 01:52:43 -0300

    Shared consumer

--- src/consumer.js
+++ src/consumer.js
@@ -1,17 +1,26 @@
+const { inspect } = require('util');
+
 const client = require('./client');
 
-const main = async () => {
-  const consumer = await client.subscribe({
-    topic: 'orders',
-    subscription: 'my-subscription',
-  });
-  console.log('Subscribed to topic');
-
-  while(true){
+const consumeMessages = async consumer => {
+  while (true) {
     const msg = await consumer.receive();
     console.log(msg.getData().toString());
     consumer.acknowledge(msg);
   }
-};
+}
 
-main()
+client.subscribe({
+  topic: 'orders',
+  subscription: 'sub_1',
+  subscriptionType: 'Shared'
+})
+  .catch(e => {
+    console.error(`Error when creating producer: ${inspect(e)}`);
+    process.exit(1);
+  })
+  .then(consumer => {
+    console.log('Subscribed to topic');
+    consumeMessages(consumer);
+  }).catch(e => console.error(inspect(e)))
+
--- src/producer.js
+++ src/producer.js
@@ -1,24 +1,34 @@
+const { inspect } = require('util');
+const { v4: uuidv4 } = require('uuid');
+
 const client = require('./client');
+
+const MESSAGE_DELAY_MS = 2000;
 
 const sleep = (ms) => new Promise(resolve => {
   setTimeout(() => resolve(), ms);
 });
 
-const main = async () => {
-  // Create a producer
-  const producer = await client.createProducer({
-    topic: 'orders',
-  });
-  
-  while(true){
-    const message = JSON.stringify({ status: 'ok' });
+const produceMessages = async producer => {
+  while (true) {
+    const message = JSON.stringify({ id: uuidv4() });
     console.log(`Sending message: ${message}`)
     await producer.send({
       data: Buffer.from(message),
     });
-    await sleep(5000);
+    await sleep(MESSAGE_DELAY_MS);
   }
-};
+}
 
-main();
-  
+client.createProducer({
+  topic: 'orders',
+})
+  .catch(e => {
+    console.error(`Error when creating producer: ${inspect(e)}`);
+    process.exit(1);
+  })
+  .then(producer => {
+    console.log('Producer created');
+    return produceMessages(producer);
+  })
+  .catch(e => console.error(inspect(e)));
